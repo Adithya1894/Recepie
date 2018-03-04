25978fad4469dec50ce940adce7b49a02e4dd273
adithya1894 <adithya1894@gmail.com>
2018-03-04 02:21:58 -0500

    initial commit

---
+++ src/main/java/com/recepie/Recepie/RecepieApplication.java
@@ -0,0 +1,12 @@
+package com.recepie.Recepie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecepieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecepieApplication.class, args);
+	}
+}
