ec07b20c6b2eafba3f47d8ac891c84f989be0435
adithya1894 <adithya1894@gmail.com>
2018-03-14 02:14:56 -0400

    Added RecepieRepository

---
+++ src/main/java/com/recepie/recepie/repositories/RecepieRepository.java
@@ -0,0 +1,9 @@
+package com.recepie.recepie.repositories;
+
+import com.recepie.recepie.domain.Recepie;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RecepieRepository extends CrudRepository<Recepie, Long>{
+
+
+}
