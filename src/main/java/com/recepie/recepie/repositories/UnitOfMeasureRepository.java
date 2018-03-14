1ffa31962a46b566c053a2b89ef69b2f9cb60dd3
adithya1894 <adithya1894@gmail.com>
2018-03-14 02:15:12 -0400

    Added UnitOfMeasureRepository

---
+++ src/main/java/com/recepie/recepie/repositories/UnitOfMeasureRepository.java
@@ -0,0 +1,8 @@
+package com.recepie.recepie.repositories;
+
+import com.recepie.recepie.domain.UnitOfMeasure;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long>{
+
+    }
